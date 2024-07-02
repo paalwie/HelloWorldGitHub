a571bc4f3e3b34851e981c7d092e78490c30faba
Jakub Waclawowicz <J.Waclawowicz@web.de>
2024-07-02 14:08:16 +0200

    change hellworld stringdf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Patrick 123 nach Erdbeeren");
+        System.out.println("Patrick 123556513152asdasdas315531sda3d1a53d1a3d1a35d nach Erdbeeren");
     }
 }
