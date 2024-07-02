abb8b5ee07332d3192ab069311cec53d6c233b7c
Jakub Waclawowicz <J.Waclawowicz@web.de>
2024-07-02 13:17:23 +0200

    change hellworld stringdf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("ddddsdgghkgfhdsfygjtfthjfhfhd wdfhgjddorld!");
+        System.out.println("Patrick stinkt nach Erdbeeren");
     }
 }
